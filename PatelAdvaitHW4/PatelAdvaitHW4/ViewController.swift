76bcb25c6066dfb8723fb084411270aebf7cc1b8
Advait Patel <advaitpa93@gmail.com>
2017-02-06 17:17:46 -0600

    adding homework 4

---
+++ PatelAdvaitHW4/PatelAdvaitHW4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PatelAdvaitHW4
+//
+//  Created by Patel, Advait Rajeshkumar on 2/6/17.
+//  Copyright © 2017 Patel, Advait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
