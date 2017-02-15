6da1d7011013173e05c7c2f5e8f9e7fbe1049c6f
Advait Patel <advaitpa93@gmail.com>
2017-02-15 16:20:13 -0600

    Adding Homework 6

---
+++ PatelAdvaitHW5/PatelAdvaitHW5/DVRViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DVRViewController.swift
+//  PatelAdvaitHW4
+//
+//  Created by Patel, Advait Rajeshkumar on 2/15/17.
+//  Copyright © 2017 Patel, Advait. All rights reserved.
+//
+
+import UIKit
+
+class DVRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PatelAdvaitHW5/PatelAdvaitHW5/ViewController.swift
+++ PatelAdvaitHW5/PatelAdvaitHW5/ViewController.swift
@@ -8,56 +8,109 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate
+{
 
     
     @IBOutlet var tf_desc: UITextField!
     @IBOutlet var tf_qty: UITextField!
+
+    @IBOutlet var tv_itemList: UITextView!
     
+    var currentItemList = ""
     
-    @IBAction func btn_newList(_ sender: UIButton)
-    {
-        //print("1")
-    }
+    @IBOutlet var viewView: UIView!
     
-    @IBAction func btn_newItem(_ sender: UIButton)
-    {
-        //print("2")
-    }
-    
-    
-    @IBAction func btn_addItem(_ sender: UIButton)
-    {
-        //print("3")
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    @IBAction func btn_newList(_ sender: UIButton)
+    {
+        print("New List")
         
-        self.view.endEditing(true)
+        currentItemList = ""
+        tv_itemList.text = "No Item"
+        tf_desc.text = ""
+        tf_qty.text=""
+        
+        UserDefaults.standard.set(currentItemList, forKey: "currentItemList")
         
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        textField.resignFirstResponder()
-        
-        return true
-        
+    @IBAction func btn_newItem(_ sender: UIButton)
+    {
+        print("Create a New Item Here")
+        tf_desc.text = ""
+        tf_qty.text=""
     }
+    
+    @IBAction func btn_addItem(_ sender: UIButton)
+    {
+        print("Add New Item Here")
+        
+        let currItemDesc = tf_desc.text!
+        let currItemQty = tf_qty.text!
+        
+        
+        tv_itemList.resignFirstResponder()
+        
+        if currItemDesc == "" {
+            let title = "Incorrect Input"
+            let message = "Item Description cant be Empty"
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let cancelAction = UIAlertAction(title: "OK",style: .cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            present(alertController,
+                    animated: true,
+                    completion: nil)
+        }else if currItemQty == ""{
+            let title = "Error, Incorrect Input"
+            let message = "Item Quantity can't be Empty"
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let cancelAction = UIAlertAction(title: "OK",style: .cancel,handler: nil)
+            alertController.addAction(cancelAction)
+            present(alertController,
+                    animated: true,
+                    completion: nil)
+        }else{
+            
+            if let _ = Int(currItemQty){
+                currentItemList = currentItemList + "\(currItemDesc) x \(currItemQty)\n"
+                tv_itemList.text=currentItemList
+                UserDefaults.standard.set(currentItemList, forKey: "currentItemList")
+            }else{
+                let title = "Error, Incorrect Input!"
+                let message = "Integer Value Required!"
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+                let cancelAction = UIAlertAction(title: "OK",style: .cancel,handler: nil)
+                alertController.addAction(cancelAction)
+                present(alertController,
+                        animated: true,
+                        completion: nil)
+            }
+            
+        }
+    }
+    
+    
     
 }
 
